744aa4ddf621d201f4ab7fac782db2556b6f2441
Colin Lin <colincool100@gmail.com>
2020-10-27 15:40:13 -0400

    added support for 4anime

--- foreground.js
+++ foreground.js
@@ -1,27 +1,40 @@
-
-var title = document.getElementsByClassName("page_title")[1].innerHTML
-var episode = title.substring(title.indexOf('Episode'),title.length)
-title = title.substring(0,title.indexOf('Episode')-1)
-console.log("Anime update tracker active")
-console.log(episode)
+var url = window.location.href
+var site
+// initialize vars according to the site
+if(url.indexOf('anime-update')>=0){
+  site="animeUpdate"
+  var title = document.getElementsByClassName("page_title")[1].innerHTML
+  var episode = title.substring(title.indexOf('Episode'),title.length)
+  title = title.substring(0,title.indexOf('Episode')-1)
+  console.log("Anime update tracker active")
+  // set up listener for next episode
+  var nextButton = document.getElementsByClassName('glyphicon glyphicon-arrow-right')[0]
+}
+else if(url.indexOf('4anime')>=0){
+  site="fourAnime"
+  var title = document.querySelector(".singletitletop a").innerHTML
+  var episode = document.querySelectorAll('#titleleft')[1].innerHTML
+  console.log("4anime tracker active")
+  // two buttons for mobile/desktop
+  var nextButtons = document.querySelectorAll('.anipager-next a')
+  var previousButtons = document.querySelectorAll('.anipager-prev a')
+  var episodeButtons = document.querySelectorAll('.episodes li a')
+  var toTrackButtons = [...nextButtons,...previousButtons,...episodeButtons]
+}
+// common vars
 var time ="00:00"
 var totalTime ="00:00"
-var url = window.location.href
 
-var baseURL = url.substring(0,url.indexOf('view')+5)
-var videoId = url.substring(url.indexOf('view')+5, url.length-1)
-chrome.storage.sync.set({['baseURL']:baseURL}, function() {
-  console.log("stored "+ baseURL)
-});
 
-// set up listener for next episode
-let nextButton = document.getElementsByClassName('glyphicon glyphicon-arrow-right')[0]
 // check if next episode button exists
-if (nextButton != undefined){
-  nextButton=nextButton.parentElement
-  nextButton.addEventListener('click',()=>{
-    chrome.runtime.sendMessage({action:'nextEpisode'},(response)=>{});
-  })
+if (toTrackButtons != undefined){
+  if(site == "fourAnime"){
+    for(button of toTrackButtons){
+      button.addEventListener('click',()=>{
+        chrome.runtime.sendMessage({action:'nextEpisode'},(response)=>{});
+      },false)
+    }
+  }
 }
 
 function remover(arr,prop,value){
@@ -36,21 +49,29 @@ function remover(arr,prop,value){
 }
 
 setInterval(function() {
+  if(site == 'animeUpdate'){
     time = document.getElementsByClassName('plyr__controls__item plyr__time--current plyr__time')[0].innerHTML
     totalTime = document.getElementsByClassName('plyr__controls__item plyr__time--duration plyr__time')[0].innerHTML
+  }
+  else if(site == 'fourAnime'){
+    time = document.querySelector('.vjs-current-time-display').innerHTML
+    time = time.substring(time.lastIndexOf('>')+1)
+    totalTime = document.querySelector('.vjs-duration-display').innerHTML
+    totalTime = totalTime.substring(totalTime.lastIndexOf(' ')+1)
+  }
     console.log(time)
     let toStore=[]
     var toStoreLW=[]
     chrome.storage.sync.get(title, function(series){
       if(series[title] ==  undefined){
         console.log("new")
-        toStore.push({"episode":episode,"time":time,"totalTime":totalTime, "videoId":videoId})
+        toStore.push({"episode":episode,"time":time,"totalTime":totalTime, "url":url})
       }
       else{
         // remove existing episode entry
         remover(series[title],'episode',episode)
         // insert new episode
-        series[title].push({"episode":episode,"time":time,"totalTime":totalTime, "videoId":videoId})
+        series[title].push({"episode":episode,"time":time,"totalTime":totalTime, "url":url})
         toStore=series[title]
         console.log(series[title])
         console.log("old")
