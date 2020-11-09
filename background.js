2083ff4511efcc3f9a47b4739cb3877eeea2f204
Colin Lin <colincool100@gmail.com>
2020-11-09 14:58:25 -0500

    fixed formating and '-' issue for anime update

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 let animeUpdate = /^https:\/\/anime-update.*/
 let fourAnime = /^https:\/\/4anime\.to.*\//
-var sites ={'animeUpdate':animeUpdate,'fourAnime':fourAnime}
+var sites = { 'animeUpdate': animeUpdate, 'fourAnime': fourAnime }
 var nextEpisode = false
 
-chrome.tabs.onActivated.addListener(tab=>{
-    chrome.tabs.get(tab.tabId,tab_info=>{        
+chrome.tabs.onActivated.addListener(tab => {
+    chrome.tabs.get(tab.tabId, tab_info => {
     })
 })
 
@@ -23,8 +23,8 @@ chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
  * 
  * @param {string} script The name of the script to be injected
  */
-function injector(script){
-    chrome.tabs.executeScript(null,{file:script},()=>console.log('i injected'))
+function injector(script) {
+    chrome.tabs.executeScript(null, { file: script }, () => console.log('i injected'))
 }
 
 /**
@@ -32,103 +32,64 @@ function injector(script){
  * 
  * @param {string} url 
  */
-function checkURL(url){
-    for(site in sites){
-        if(sites[site].test(url)){
+function checkURL(url) {
+    for (site in sites) {
+        if (sites[site].test(url)) {
             return true
         }
     }
     return false
 }
 
-chrome.runtime.onMessage.addListener( async(request,sender,sendResponse)=>{
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     let action = request.action
     let response
-    if(action === 'track'){
+    if (action === 'track') {
         // inject foreground.js into the current tab when user clicks "Start Tracking"
         chrome.tabs.getSelected(null, (tab) => {
-            if(checkURL(tab.url)){
+            if (checkURL(tab.url)) {
                 console.log("ran")
                 injector('./foreground.js')
             }
         });
-        response ='tracking'
+        response = 'tracking'
     }
-    else if (action === 'resume'){
-        chrome.storage.sync.get(null, function(result){
+    else if (action === 'resume') {
+        chrome.storage.sync.get(null, function (result) {
             let url = result[request.title].url
             let timeText = result[request.title].time
 
             // convert timeText to time in seconds
-            let time = parseInt(timeText.substring(0,timeText.indexOf(":"))) * 60
-            time = time + parseInt(timeText.substring(timeText.indexOf(":")+1))
+            let time = parseInt(timeText.substring(0, timeText.indexOf(":"))) * 60
+            time = time + parseInt(timeText.substring(timeText.indexOf(":") + 1))
 
             let videoElement
             let delay = 1000
 
             // set appropriate CSS selector for video element
-            if(url.indexOf('anime-update')>=0){
-                videoElement='video'
+            if (url.indexOf('anime-update') >= 0) {
+                videoElement = 'video'
                 delay = 2000
             }
-            else if (url.indexOf('4anime')>=0){
+            else if (url.indexOf('4anime') >= 0) {
                 videoElement = '#example_video_1_html5_api'
             }
             // open episode in new tab
-            chrome.tabs.create({ "url": url }, function (tab){
+            chrome.tabs.create({ "url": url }, function (tab) {
                 // set video to where user last left off
-                setTimeout(function(){chrome.tabs.executeScript(null,{code:`document.querySelector('${videoElement}').currentTime=${time}`})}, delay)
+                setTimeout(function () { chrome.tabs.executeScript(null, { code: `document.querySelector('${videoElement}').currentTime=${time}` }) }, delay)
                 // start tracking the current episode
-                setTimeout(function(){injector('./foreground.js')}, delay)
+                setTimeout(function () { injector('./foreground.js') }, delay)
             })
-          });
+        });
 
-        response ='resumed'
+        response = 'resumed'
     }
-    else if(action === 'nextEpisode'){
+    else if (action === 'nextEpisode') {
         // trigger nextEpisode event
         console.log('nextEpisode')
-        nextEpisode=true
-        response='nextEpisode'
+        nextEpisode = true
+        response = 'nextEpisode'
     }
-    sendResponse({action:response});
+    sendResponse({ action: response });
 })
-
-// mal_ids of anime series
-var malIDs = {}
-var daysOfWeek = ['monday', 'tuesday','wednesday', 'thursday','friday','saturday','sunday']
-
-updateMalIDs = () => {
-    chrome.storage.sync.get(['malIDs'], function(result){
-    // if malIDs doesn't exist or its monday (1), create/update it  
-    let d = new Date()
-    if (result['malIDs'] == undefined || d.getDay() == 1){
-        fetch("https://api.jikan.moe/v3/schedule",{
-            method: 'GET', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            headers: {
-              'Content-Type': 'application/json'
-            }})
-        .then(res=> res.json()) // convert json to JS object
-        .then(data =>{
-            for(key in data){
-                if(daysOfWeek.includes(key))
-                    for(obj of data[key]){
-                        // key is anime title, value is mal_id
-                        malIDs[obj['title']] = obj['mal_id']
-                    }
-            }
-            chrome.storage.sync.set({['malIDs']:malIDs}, function() {
-                console.log('updated mal_ids')
-            });
-        })
-    }
-    else{
-        console.log(result['malIDs'])
-    }
-
-    });
-}
-updateMalIDs() 
-// run this every 24 hours
-setInterval(updateMalIDs, 24 * 60 * 60 * 1000);
--- foreground.js
+++ foreground.js
@@ -2,16 +2,16 @@ var url = window.location.href
 var site
 
 // initialize vars according to the site
-if(url.indexOf('anime-update')>=0){
-  site="Anime Update"
+if (url.indexOf('anime-update') >= 0) {
+  site = "Anime Update"
   let fullName = document.getElementsByClassName("page_title")[1].innerHTML
   // Values for OVAs and Movies
   var episode = ""
   var title = fullName
   // Values for an episode of a series
-  if(fullName.indexOf('Episode')>=0){
-    episode = fullName.substring(fullName.indexOf('Episode'),fullName.length)
-    title = fullName.substring(0,fullName.indexOf('Episode')-1)
+  if (fullName.indexOf('Episode') >= 0) {
+    episode = fullName.substring(fullName.indexOf('Episode'), fullName.length)
+    title = fullName.substring(0, fullName.indexOf('Episode') - 1)
   }
 
   console.log("Anime update tracker active")
@@ -19,29 +19,32 @@ if(url.indexOf('anime-update')>=0){
   let baseURL = 'https://anime-update.com/watch-online/'
 
   // for some reason hunter x hunter doesn't follow this pattern
-  if (fullName.indexOf('Hunter x Hunter')>=0) 
-    fullName=fullName.replace('Episode ','');
-  
+  if (fullName.indexOf('Hunter x Hunter') >= 0)
+    fullName = fullName.replace('Episode ', '');
+
+  // - in titles causes formatting error
+  if (fullName.indexOf('-') >=0)
+    fullName = fullName.replace(' - ',' ')
 
   /* Generate url from fullName */
 
   fullName = fullName.toLowerCase()
   // remove all special characters
-  fullName = fullName.replace(/[^a-zA-Z0-9 ]/g,'')
+  fullName = fullName.replace(/[^a-zA-Z0-9 ]/g, '')
   // replace ' '  with '-'
-  fullName = fullName.replace(/ /g,'-')
+  fullName = fullName.replace(/ /g, '-')
   // remove trailing '-'
-  url =baseURL+fullName.substring(0,fullName.length-1)
+  url = baseURL + fullName.substring(0, fullName.length - 1)
 
   // next and previous buttons
   let nextButton = document.querySelector('.glyphicon.glyphicon-arrow-right')
   let previousButton = document.querySelector('.glyphicon.glyphicon-arrow-left')
-  nextButton = nextButton != undefined? nextButton.parentElement:nextButton
-  previousButton = previousButton != undefined? previousButton.parentElement:previousButton
-  var toTrackButtons = [nextButton,previousButton]
+  nextButton = nextButton != undefined ? nextButton.parentElement : nextButton
+  previousButton = previousButton != undefined ? previousButton.parentElement : previousButton
+  var toTrackButtons = [nextButton, previousButton]
 }
-else if(url.indexOf('4anime')>=0){
-  site="4anime"
+else if (url.indexOf('4anime') >= 0) {
+  site = "4anime"
   var title = document.querySelector(".singletitletop a").innerHTML
   var episode = document.querySelectorAll('#titleleft')[1].innerHTML
 
@@ -51,21 +54,21 @@ else if(url.indexOf('4anime')>=0){
   let nextButtons = document.querySelectorAll('.anipager-next a')
   let previousButtons = document.querySelectorAll('.anipager-prev a')
   let episodeButtons = document.querySelectorAll('.episodes li a')
-  var toTrackButtons = [...nextButtons,...previousButtons,...episodeButtons]
+  var toTrackButtons = [...nextButtons, ...previousButtons, ...episodeButtons]
 }
 
 // common vars
-var time ="00:00"
-var totalTime ="00:00"
+var time = "00:00"
+var totalTime = "00:00"
 
 // check if next episode buttons exists
-if (toTrackButtons != undefined){
+if (toTrackButtons != undefined) {
   // add an event listener to each button which triggers the nextEpisode event
-  for(button of toTrackButtons){
-    if(button != undefined){
-      button.addEventListener('click',()=>{
-        chrome.runtime.sendMessage({action:'nextEpisode'},(response)=>{});
-      },false)
+  for (button of toTrackButtons) {
+    if (button != undefined) {
+      button.addEventListener('click', () => {
+        chrome.runtime.sendMessage({ action: 'nextEpisode' }, (response) => { });
+      }, false)
     }
   }
 }
@@ -75,10 +78,10 @@ if (toTrackButtons != undefined){
  * @param {array} arr The array to remove from
  * @param {string} value The value of the element to be removed
  */
-function remover(arr,value){
-  for(let i = 0;i<arr.length;i++){
-    if(arr[i]=== value){
-      arr.splice(i,1)
+function remover(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      arr.splice(i, 1)
       console.log("removed")
     }
   }
@@ -90,78 +93,78 @@ function remover(arr,value){
  * 
  * @param {string} videoPlayer The name of the video player 
  */
-function setTime(videoPlayer){
+function setTime(videoPlayer) {
   let timeElement
   let totalTimeElement
 
-  if(videoPlayer == "Video"){
-    timeElement=".plyr__controls__item.plyr__time--current.plyr__time"
-    totalTimeElement=".plyr__controls__item.plyr__time--duration.plyr__time"
+  if (videoPlayer == "Video") {
+    timeElement = ".plyr__controls__item.plyr__time--current.plyr__time"
+    totalTimeElement = ".plyr__controls__item.plyr__time--duration.plyr__time"
   }
   // Other video players wont work because they are iframes
   // Can't get DOM elements of cross-domain iframe
-  else if( videoPlayer == "MX"){
+  else if (videoPlayer == "MX") {
     timeElement = ".vjs-current-time-display"
     totalTimeElement = ".vjs-duration-display"
   }
-  else if( videoPlayer == "VCDN" || videoPlayer == "Viz" ){
+  else if (videoPlayer == "VCDN" || videoPlayer == "Viz") {
     timeElement = ".jw-icon.jw-icon-inline.jw-text.jw-reset.jw-text-elapsed"
     totalTimeElement = ".jw-icon.jw-icon-inline.jw-text.jw-reset.jw-text-duration"
-    
+
   }
   time = document.querySelector(timeElement).innerHTML
   totalTime = document.querySelector(totalTimeElement).innerHTML
 
 }
 
-setInterval(function() {
+setInterval(function () {
   // get time from video players
-  if(site == 'Anime Update'){
+  if (site == 'Anime Update') {
     let videoPlayer = document.querySelector(".nav.nav-tabs.hometab .active a").innerHTML
-    console.log("time "+videoPlayer)
+    console.log("time " + videoPlayer)
     // sets time and totalTime from video player data 
     setTime(videoPlayer)
   }
-  else if(site == '4anime'){
+  else if (site == '4anime') {
     time = document.querySelector('.vjs-current-time-display').innerHTML
-    time = time.substring(time.lastIndexOf('>')+1)
+    time = time.substring(time.lastIndexOf('>') + 1)
     totalTime = document.querySelector('.vjs-duration-display').innerHTML
-    totalTime = totalTime.substring(totalTime.lastIndexOf(' ')+1)
+    totalTime = totalTime.substring(totalTime.lastIndexOf(' ') + 1)
   }
-    let toStore
-    var toStoreLW=[]
+  let toStore
+  var toStoreLW = []
 
-    // store current episode in storage
-    toStore = {"episode":episode,"time":time,"totalTime":totalTime, "url":url, "site":site}
-    chrome.storage.sync.set({[title]:toStore}, function() {
-      console.log('added ' + title+" "+episode+" "+time+"/"+totalTime)
+  // store current episode in storage
+  toStore = { "episode": episode, "time": time, "totalTime": totalTime, "url": url, "site": site }
+  chrome.storage.sync.set({ [title]: toStore }, function () {
+    console.log('added ' + title + " " + episode + " " + time + "/" + totalTime)
+  });
+
+  // update last watched
+  chrome.storage.sync.get(['lastWatched'], function (result) {
+    if (result['lastWatched'] == undefined) {
+      toStoreLW.push(title)
+    }
+    else {
+      // remove existing series entry
+      remover(result['lastWatched'], title)
+      // insert new series to end of array
+      result['lastWatched'].push(title)
+      toStoreLW = result['lastWatched']
+    }
+    chrome.storage.sync.set({ ['lastWatched']: toStoreLW }, function () {
+      console.log('last watched ' + title + " " + episode + " " + time + "/" + totalTime)
     });
+  });
 
-    // update last watched
-    chrome.storage.sync.get(['lastWatched'], function(result){
-      if(result['lastWatched'] == undefined){
-        toStoreLW.push(title)
-      }
-      else{
-        // remove existing series entry
-        remover(result['lastWatched'],title)
-        // insert new series to end of array
-        result['lastWatched'].push(title)
-        toStoreLW = result['lastWatched']
-      }
-      chrome.storage.sync.set({['lastWatched']:toStoreLW}, function() {
-        console.log('last watched ' +title+" "+episode+" "+time+"/"+totalTime)
-      });
-    });
+  // connect to port info
+  let port = chrome.runtime.connect({ name: "info" });
 
-    // connect to port info
-    let port = chrome.runtime.connect({name: "info"});
+  // pass data for popup.js to display
+  port.postMessage({ title: title, episode: episode, time: time, totalTime: totalTime, site: site, action: 'tracking' });
+  port.onMessage.addListener(function (msg) { });
 
-    // pass data for popup.js to display
-    port.postMessage({title:title, episode:episode, time:time, totalTime:totalTime, site:site, action:'tracking'});
-    port.onMessage.addListener(function(msg) {});
-    
-}, 5*1000);
+}, 5 * 1000);
 
 
 
